c83c7f459f39c9c508767432a774cb9e8a0de2a1
nikibhatt <nkibhatt@users.noreply.github.com>
2020-04-21 16:15:01 -0700

    completed assignment

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('../doubly_linked_list')
-from doubly_linked_list import DoublyLinkedList
+from doubly_linked_list import DoublyLinkedList, ListNode
 
 
 class Queue:
@@ -8,12 +8,38 @@ class Queue:
         self.size = 0
         # Why is our DLL a good choice to store our elements?
         # self.storage = ?
+        self.node = ListNode(None)
+        self.dll = DoublyLinkedList(self.node)
 
     def enqueue(self, value):
-        pass
+        if self.dll.head.value is None:
+            self.dll.head.value = value
+        else:
+            self.dll.add_to_tail(value)
 
     def dequeue(self):
-        pass
+        if self.dll:
+            return(self.dll.remove_from_head())
+        else:
+            return None
+
 
     def len(self):
-        pass
+        if self.dll:
+            if self.dll.head.value is None:
+                return 0
+            else:
+                return(self.dll.length)
+        else:
+            return 0
+
+
+test_q = Queue()
+print(test_q.len())
+test_q.enqueue(3)
+test_q.enqueue(5)
+test_q.enqueue(7)
+test_q.dequeue()
+test_q.dequeue()
+test_q.dequeue()
+print(test_q.dequeue())
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -1,18 +1,32 @@
 import sys
 sys.path.append('../doubly_linked_list')
-from doubly_linked_list import DoublyLinkedList
+from doubly_linked_list import DoublyLinkedList, ListNode
 
 class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
         # self.storage = ?
+        self.node = ListNode(None)
+        self.dll = DoublyLinkedList(self.node)
 
     def push(self, value):
-        pass
+        if self.dll.head.value is None:
+            self.dll.head.value = value
+        else:
+            self.dll.add_to_head(value)
 
     def pop(self):
-        pass
+        if self.dll:
+            return(self.dll.remove_from_head())
+        else:
+            return None
 
     def len(self):
-        pass
+        if self.dll:
+            if self.dll.head.value is None:
+                return 0
+            else:
+                return(self.dll.length)
+        else:
+            return 0
